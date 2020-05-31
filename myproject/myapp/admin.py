0b610390e9bcacc45f87fe70940fa5277743d7ed
AdityaChavan27 <aac84@njit.edu>
2020-05-31 15:58:25 -0400

    made use of foreign keys in models

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from myapp.models import login
+from myapp.models import session
+from myapp.models import student
+admin.site.register(login)
+admin.site.register(session)
+admin.site.register(student)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class login(models.Model):
+    Name=models.CharField(max_length=30)
+    Password = models.CharField(max_length=20)
+    Email = models.EmailField(max_length=30)
+
+    def __str__(self):
+        return self.Name
+
+class session(models.Model):
+    session=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.session
+
+class student(models.Model):
+    name=models.CharField(max_length=30)
+    session=models.ForeignKey("session",on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
